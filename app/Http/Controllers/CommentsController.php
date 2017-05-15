9ba680dfa8c5b4d4368664e7513b6d97abd0e5cb
Interview <interview@simons-mbp.home>
2017-05-15 12:44:09 +0100

    adds comments controller and comments model

---
+++ app/Http/Controllers/CommentsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommentsController extends Controller
+{
+    //
+
+
+    function index() {
+        return view('homepage');
+    }
+}
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <title>Guest Book</title>
+    </head>
+    <body>
+        <h1>Guest Book</h1>
+    </body>
+</html>
